dfc845b29b18c1244d4d5a0c4c8f5278f5799cba
Marvellous Ogunode <ogunodemarvellous@gmail.com>
2025-11-16 09:16:46 +0100

    Implement SubstrateConnection singleton class

---
+++ web-interface/src/services/substrate/connection.ts
@@ -0,0 +1,23 @@
+export class SubstrateConnection {
+  private static instance: SubstrateConnection;
+  private wsEndpoint: string = import.meta.env.VITE_PARACHAIN_WS || 'ws://localhost:9944';
+
+  private constructor() {}
+
+  static getInstance(): SubstrateConnection {
+    if (!SubstrateConnection.instance) {
+      SubstrateConnection.instance = new SubstrateConnection();
+    }
+    return SubstrateConnection.instance;
+  }
+
+  async connect() {
+    // Simulates connection to Substrate node
+    // In production: use @polkadot/api
+    return { connected: true, endpoint: this.wsEndpoint };
+  }
+
+  getEndpoint(): string {
+    return this.wsEndpoint;
+  }
+}
